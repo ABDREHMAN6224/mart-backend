3c8a059ee788ed7e44cffb846d30db32a9dd35fe
raja <memonabdulrehman250@gmail.com>
2023-07-01 15:06:20 +0500

    products update

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 require("dotenv").config()
-// This is your test secret API key.
 const express = require('express');
 const app = express();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const bodyParser = require("body-parser");
 const cors=require("cors")
+
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
-app.get("/",(req,res)=>{
-  res.send("hello")
+
+const data=[]
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base('appqzmYk6pQwf1HEh');
+
+
+
+app.get("/products",(req,res)=>{
+  base('products').select({
+      view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+     
+  
+      records.forEach(function(record) {
+        data.push({id:record.id,...record.fields})
+          console.log('Retrieved', record.fields);
+      });
+  
+      fetchNextPage();
+  
+  }, function done(err) {
+      if (err) { console.error(err); return; }
+  });
+  res.send(data)
+})
+app.get("/products/:id",(req,res)=>{
+base('products').find(req.params.id, function(err, record) {
+    if (err) { console.error(err); return; }
+    res.send({id:record.id,...record.fields})
+});
 })
 app.get("/config",(req,res)=>{
   res.send({publishableKey:process.env.STRIPE_PUBLIC_KEY})
